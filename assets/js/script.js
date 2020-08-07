003e6a3e87d92529de325cb65eb69b064e28e33e
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2020-08-07 22:39:53 +0530

    0.4.6
    Changelog:
    New Nav
    Updated Colors
    Added Footbar

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".nav-text");
-    var $navi = $(".nav-icn");
-    var $wall = $("#Wall")
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $wall.height());
-    $navi.toggleClass('scrolled', $(this).scrollTop() > $wall.height());
-  });
-});
